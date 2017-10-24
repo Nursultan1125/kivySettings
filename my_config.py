dd02ceff1522f1ee3ee020075f0d8a0886be22c9
Nursultan1125 <zholnurs@gmail.com>
2017-10-24 23:46:26 +0600

    add files

---
+++ my_config.py
@@ -0,0 +1,61 @@
+json = '''
+[
+    {
+        "type": "numeric",
+        "title": "Led0",
+        "desc": "",
+        "section": "My Label",
+        "key": "led0"
+    },
+    {
+        "type": "bool",
+        "title": "Led1",
+        "desc": "",
+        "section": "My Label",
+        "key": "led1"
+    },
+    {
+        "type": "bool",
+        "title": "Led2",
+        "desc": "",
+        "section": "My Label",
+        "key": "led2"
+    },
+    {
+        "type": "bool",
+        "title": "Led3",
+        "desc": "",
+        "section": "My Label",
+        "key": "led3"
+    },
+    {
+        "type": "bool",
+        "title": "Led4",
+        "desc": "",
+        "section": "My Label",
+        "key": "led4"
+    },
+    {
+        "type": "bool",
+        "title": "Led5",
+        "desc": "",
+        "section": "My Label",
+        "key": "led5"
+    },
+    {
+        "type": "bool",
+        "title": "Led6",
+        "desc": "",
+        "section": "My Label",
+        "key": "led6"
+    },
+    {
+        "type": "bool",
+        "title": "Led7",
+        "desc": "",
+        "section": "My Label",
+        "key": "led7"
+    }
+    
+]
+'''
